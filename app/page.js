c728784fd456c4fe79fe62ee47ec76bb46e77a28
Sambhav Rana <sambhavrana27@gmail.com>
2025-01-08 14:36:42 +0530

    Setting up Shadcn + App Layout

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      AI Career Coach
+      <br />
+      <Button>START</Button>
+    </>
+  );
+}
---
+++ components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+
+            HEADER
+        </div>
+    )
+}
+
+export default Header
