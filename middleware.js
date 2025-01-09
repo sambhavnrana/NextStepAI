77e65355bd1ee9bc8f3ba6f001717487d1b336cf
Sambhav Rana <sambhavrana27@gmail.com>
2025-01-09 11:19:52 +0530

    Fix: Corrected redirectToSignIn usage in middleware file

--- middleware.js
+++ middleware.js
@@ -13,8 +13,8 @@ export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignin } = await auth();
-    return redirectToSignin();
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
   }
 
   return NextResponse.next();
