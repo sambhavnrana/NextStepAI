8bbfa1b919bae1dd4a8c3eb44053dfe49c81c7a7
Sambhav Rana <sambhavrana27@gmail.com>
2025-01-09 11:15:24 +0530

    feat: create DB Models + store user in DB

--- components/header.jsx
+++ components/header.jsx
@@ -4,9 +4,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react';
 import { Button } from './ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { checkUser } from '@/lib/checkUser.js';
 
-const Header = () => {
+const Header = async () => {
+    await checkUser();
     return (
         <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
             <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
@@ -76,7 +78,9 @@ const Header = () => {
                                     userButtonPopoverCard: 'shadow-xl',
                                     userPreviewMainIdentifier: 'font-semibold',
                                 }
-                            }} />
+                            }}
+                            afterSignOutUrl='/'
+                        />
                     </SignedIn>
                 </div>
             </nav>
