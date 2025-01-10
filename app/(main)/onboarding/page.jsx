f9f2b0bdc1d7dcb7f0a78b6038a3923d90cb2bb8
Sambhav Rana <sambhavrana27@gmail.com>
2025-01-10 18:27:54 +0530

    feat: add Onboarding page + Onboarding APIs

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { industries } from "@/data/industries"
+import { getUserOnboardingStatus } from "@/actions/user"
+import { redirect } from "next/navigation"
+import OnboardingForm from "./_components/onboarding-form"
 
-const OnboardingPage = () => {
+
+const OnboardingPage = async () => {
+
+    const { isOnboarded } = await getUserOnboardingStatus();
+
+    if (isOnboarded) {
+        redirect('/dashboard')
+    }
     return (
-        <div>
-            Onboarding
-        </div>
+        <main>
+            <OnboardingForm industries={industries} />
+        </main>
     )
 }
 
