47eabe53f7dee1742eb290326d608e2012a65316
Sambhav Rana <sambhavrana27@gmail.com>
2025-01-14 21:53:29 +0530

    feat: add Cron for weekly insights (Inngest functions)

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client.js";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     /* your functions will be passed here later! */
+    generateIndustryInsights,
   ],
 });
